be43d778713ef18a15e135432de4eddbcd2157ca
Abdulaziz <abdulaziztubah@gmail.com>
2021-11-30 21:06:07 +0200

    End of section 2

---
+++ API/Controllers/PetsController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PetsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PetsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppPet>>> GetPets()
+        {
+            return await _context.Pets.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppPet>> GetPet(int id)
+        {
+            return await _context.Pets.FindAsync(id);
+        }
+    }
+}
